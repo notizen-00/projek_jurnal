4478c45c290857fa0dd47a2067099cf9aa35dd4f
notizen-00 <dipta.ikromi@unej.ac.id>
2023-07-20 18:16:51 +0700

    Tipe Pembelian Service

---
+++ app/Interfaces/Pembelian/TipePembelianInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Interfaces\Pembelian;
+
+interface TipePembelianInterface
+{
+    public function check($data);
+
+
+} 
---
+++ app/Services/Pembelian/Tipe/PemesananService.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Services\Pembelian\Tipe;
+
+use App\Interfaces\Pembelian\TipePembelianInterface;
+
+class PemesananService implements TipePembelianInterface
+
+{
+
+    protected $tipe;
+
+
+    public function __construct($tipe = 'pemesanan')
+    {
+        $this->tipe = $tipe;
+    }
+
+
+   
+}
